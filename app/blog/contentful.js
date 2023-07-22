f7ec90c66d37c860724c031c898b386715b7a44c
JosiahSco <jscott70@uco.edu>
2023-07-21 22:25:22 -0500

    hotfix

--- app/blog/contentful.js
+++ app/blog/contentful.js
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful';
 
 const client = createClient({
-  space: 'tyisb6127qoe',//process.env.CONTENTFUL_SPACE_ID,
-  accessToken: 'WryID0BRLPD2D45UtsVPHUCwzL3UuOHz4GiBzNynkHY'//process.env.CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
 });
 
 export default client;
