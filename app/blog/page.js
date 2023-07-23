70c8b2dc23b10b0bcef5b202281a862b2fd627e6
JosiahSco <jscott70@uco.edu>
2023-07-23 12:26:58 -0500

    blog placeholder change

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,14 +1,23 @@
 import styles from './page.module.css'
+import client from './contentful'
+
+export async function getStaticProps() {
+    const contClient = client;
+    const resp = await contClient.getEntries({ content_type: 'blogPage' });
+    console.log(resp)
+
+    return {
+        props: {
+            posts: resp.items
+        }
+    }
+}
 
 export default function Blog({ posts }) {
+    //console.log(posts)
     return (
         <main className={styles.main}>
-         <h1>This is where the blog will go</h1>
-         <p>I will display previews of all blog posts as cards. 
-            The cards will act as links that take you to josiahscott.dev/blog/postID.
-            To write and host blog content I will be using Contentful.
-        </p>
-         
+         <h1>This is where the blog post previews will go</h1>
         </main>
     )
 }
