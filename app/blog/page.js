7726f1d70caadc6c1273382ebaf6dc54292b2734
JosiahSco <jscott70@uco.edu>
2023-07-28 21:09:19 -0500

    blog styling changes

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,7 @@
 import styles from './page.module.css'
 import client from './contentful';
 import Link from 'next/link';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 async function getPosts() {
     const contClient = client;
@@ -10,21 +11,32 @@ async function getPosts() {
 
 export default async function Blog() {
     const posts = await getPosts();
+    console.log(posts[0].fields.preview)
     return (
         <main className={styles.main}>
          {/* <h1>Blog Posts</h1> */}
          <div className={styles.postPreviews}>
          {posts.map(post => (
-            <Link key={post.fields.slug} href={'blog/' + post.fields.slug}>
+            // <Link key={post.fields.slug} href={'blog/' + post.fields.slug}>
             <div className={styles.post} key={post.sys.id}>
+                <div className={styles.postLeft}>
                 <h2>
                 {post.fields.title}
                 </h2>
                 <p>{post.fields.date}</p>
                 <p>{}</p>
+                </div>
+                <div className={styles.postRight}>
+                    <div className={styles.previewText}>
+                        {post.fields.preview}...
+                    </div>
+                    <Link className={styles.readMore} href={'blog/' + post.fields.slug}>
+                        Read More
+                    </Link>
+                </div>
                 
             </div>
-            </Link>
+            //</Link>
          ))}
          </div>
         </main>
