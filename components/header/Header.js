dd30711a9a055c0c5616ec22c326f1fa9fb9fbd5
JosiahSco <jscott70@uco.edu>
2023-09-27 21:20:21 -0500

    click outside of expanded nav menu to close it

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,12 +1,12 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Link from "next/link"
 import styles from './header.css'
 import Image from "next/image"
 
 export default function Header() {
 
-
+    const navRef = useRef(null);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -26,10 +26,27 @@ export default function Header() {
         };
       }, []);
     
-
+      const handleClickOutside = (event) => {
+        if (navRef.current && !navRef.current.contains(event.target)) {
+          setIsMenuOpen(false);
+        }
+      };
+    
+      // Add an event listener to handle clicks outside of the menu
+      useEffect(() => {
+        if (isMenuOpen) {
+          document.addEventListener('mousedown', handleClickOutside);
+        } else {
+          document.removeEventListener('mousedown', handleClickOutside);
+        }
+    
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, [isMenuOpen]);
 
     return (
-        <nav className='navigation'>
+        <nav className='navigation' ref={navRef}>
             {/* <div id="navContainer"> */}
                 <button className="menu-toggle" onClick={toggleMenu}>☰</button>
                 <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
