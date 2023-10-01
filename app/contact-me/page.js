45f2f9ced9c5a37e423be6a5e0731f7aca1534be
JosiahSco <jscott70@uco.edu>
2023-10-01 18:51:12 -0500

    contact page updates

--- app/contact-me/page.js
+++ app/contact-me/page.js
@@ -1,7 +1,9 @@
 "use client"
 import { useState } from 'react'
-import styles from './page.module.css'
+import styles from './contact.css'
+// import styles from './page.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function ContactMe() {
     const [formData, setFormData] = useState({
@@ -37,66 +39,79 @@ export default function ContactMe() {
     }
 
     return (
-        <main className={styles.main}>
-            <div className={styles.outerCard}>
-                <div className={styles.cardContent}>
-                    <div className={styles.leftWrapper}>
-                        <h1>Useful Links</h1>
-                        <div className={styles.links}>
-                            <a href='https://github.com/JosiahSco' target='_blank'>
+        <main className="main">
+            <div className="leftWrapper">
+                <div className='contactInfo'>
+                    <h1>Contact Info</h1>
+                    <div className='icon-and-address'>
+                        <div className='emailIcon'>
+                            <Link href="mailto: josiahscottdev@gmail.com">
                                 <Image 
-                                    src='/github-pages.png'
+                                    src='/emailIcon.svg'
                                     fill={true}
-                                    alt='Github Logo'
+                                    alt='Email Icon'
                                     sizes='(max-width: 768px) 75px, (max-width: 1200px) 125px'
                                     priority
                                 />
-                            </a>
-                            <a href='https://www.linkedin.com/in/josiah-scott-379b08238/' target='_blank'>
-                                <Image 
-                                    src='/linkedin-tile.svg'
-                                    fill={true}
-                                    alt='LinkedIn Logo'
-                                    sizes='(max-width: 768px) 75px, (max-width: 1200px) 125px'
-                                    priority
-                                />
-                            </a>
-                            <a href='https://www.discordapp.com/users/245749938686787584' target='_blank'>
-                                <Image 
-                                    src='/discord2.png'
-                                    fill={true}
-                                    alt='Discord Logo'
-                                    sizes='(max-width: 768px) 75px, (max-width: 1200px) 125px'
-                                    priority
-                                />
-                            </a>
+                            </Link>
                         </div>
+                        <p>josiahscottdev@gmail.com</p>
                     </div>
-                    <div className={styles.rightWrapper}>
-                        <h2>Send Me a Message</h2>
-                        <div className={styles.formWrapper}>
-                        <form className={styles.emailForm} onSubmit={handleSubmit}>
-                            <label>
-                                <input className={styles.name} type='text' name='name' value={formData.name} onChange={handleChange} placeholder='Name' required></input>
-                            </label>
-                            <label>
-                                <input type='email' name='email' value={formData.email} onChange={handleChange} placeholder='Email' required></input>
-                            </label>
-                            <label>
-                                <textarea className={styles.message} name='message' value={formData.message} onChange={handleChange} placeholder='Your Message' required></textarea>
-                            </label>
-                            <button type='submit'>Send Message</button>
-                        </form>
-                        <dialog id='messageSentModal' className={styles.messageSentModal}>
-                            <div>
-                                <h3>Message Sent!</h3>
-                                <p> Thanks for the message!</p>
-                                <p>I will respond as soon as I can.</p>
-                            </div>
-                            <button id='closeModal' onClick={closeModal}>Close</button>
-                        </dialog>
+                </div>
+                <h1>Useful Links</h1>
+                <div className="links">
+                    <a href='https://github.com/JosiahSco' target='_blank'>
+                        <Image
+                            src='/github-pages.png'
+                            fill={true}
+                            alt='Github Logo'
+                            sizes='(max-width: 768px) 75px, (max-width: 1200px) 125px'
+                            priority
+                        />
+                    </a>
+                    <a href='https://www.linkedin.com/in/josiah-scott-379b08238/' target='_blank'>
+                        <Image
+                            src='/linkedin-tile.svg'
+                            fill={true}
+                            alt='LinkedIn Logo'
+                            sizes='(max-width: 768px) 75px, (max-width: 1200px) 125px'
+                            priority
+                        />
+                    </a>
+                    <a href='https://www.discordapp.com/users/245749938686787584' target='_blank'>
+                        <Image
+                            src='/discord2.png'
+                            fill={true}
+                            alt='Discord Logo'
+                            sizes='(max-width: 768px) 75px, (max-width: 1200px) 125px'
+                            priority
+                        />
+                    </a>
+                </div>
+            </div>
+            <div className="rightWrapper">
+                <h2>Send Me a Message</h2>
+                <div className="formWrapper">
+                    <form className="emailForm" onSubmit={handleSubmit}>
+                        <label>
+                            <input className="name" type='text' name='name' value={formData.name} onChange={handleChange} placeholder='Name' required></input>
+                        </label>
+                        <label>
+                            <input type='email' name='email' value={formData.email} onChange={handleChange} placeholder='Email' required></input>
+                        </label>
+                        <label>
+                            <textarea className="message" name='message' value={formData.message} onChange={handleChange} placeholder='Your Message' required></textarea>
+                        </label>
+                        <button type='submit'>Send Message</button>
+                    </form>
+                    <dialog id='messageSentModal' className="messageSentModal">
+                        <div>
+                            <h3>Message Sent!</h3>
+                            <p> Thanks for the message!</p>
+                            <p>I will respond as soon as I can.</p>
                         </div>
-                    </div>
+                        <button id='closeModal' onClick={closeModal}>Close</button>
+                    </dialog>
                 </div>
             </div>
         </main>
