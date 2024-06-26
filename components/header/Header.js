a12218e30860e01c5959ad7fda9b7de121bbef3c
JosiahSco <jscott70@uco.edu>
2024-06-26 11:21:21 -0500

    highlight nav link of current page

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,5 +1,6 @@
 "use client"
 import React, { useState, useEffect, useRef } from 'react';
+import { usePathname } from 'next/navigation';
 import Link from "next/link"
 import styles from './header.css'
 import Image from "next/image"
@@ -8,6 +9,7 @@ export default function Header() {
 
     const navRef = useRef(null);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const pathname = usePathname();
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
@@ -45,28 +47,32 @@ export default function Header() {
         };
       }, [isMenuOpen]);
 
+      const getLinkClass = (path) => {
+        return pathname === path ? 'active' : '';
+      };
+
     return (
         <nav className='navigation' ref={navRef}>
           <button className="menu-toggle" onClick={toggleMenu}>☰</button>
           <Link href={'/'} className='name'>Josiah Scott</Link>
           <ul className={`nav-links ${isMenuOpen ? 'open' : 'closed'}`}>
-              <li>
+              <li className={getLinkClass('/')}>
                   <Link href={'/'} id="homeLink" className='link'>Home</Link>
               </li>
               <hr></hr>
-              <li>
+              <li className={getLinkClass('/resume')}>
                   <Link href={'/resume'} id='resumeLink' className='link'>Resume</Link>
               </li>
               <hr></hr>
-              <li>
+              <li className={getLinkClass('/projects')}>
                   <Link href={'/projects'} id="projectsLink" className='link'>Projects</Link>
               </li>
               <hr></hr>
-              <li>
+              <li className={getLinkClass('/blog')}>
                   <Link href={'/blog'} id="blogLink" className='link'>Blog</Link>
               </li>
               <hr></hr>
-              <li>
+              <li className={getLinkClass('/contact-me')}>
                   <Link href={'/contact-me'} id="contactMeLink" className='link'>Contact</Link>
               </li>
           </ul>
